f9d33ee609b5f16b031a1e73dfa3da06f5bad1ec
caoyangyang <2451772768@qq.com>
2018-05-28 18:26:19 +0800

    iii

---
+++ app/src/main/java/com/project/cyy/plan/listener/OnItemChildViewClickListener.java
@@ -0,0 +1,10 @@
+package com.project.cyy.plan.listener;
+
+/**
+ * Created by cyy
+ * on 18-5-28下午6:25
+ */
+public interface OnItemChildViewClickListener {
+    void onItemChildViewClickListener(int id, int position);
+
+ }
